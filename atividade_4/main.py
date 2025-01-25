b83ea50557e3d302f96e16000c8a73d32aad51ce
Nonolism <ruanvitormoreno@outlook.com>
2025-01-25 18:49:42 -0300

    Atv4 Calculando o IMC

---
+++ atividade_4/main.py
@@ -0,0 +1,18 @@
+def main():
+    peso = float(input('Digite seu peso (kg): '))
+    altura = float(input('Digite sua altura (m): '))
+    imc = peso / (altura ** 2)
+    
+    if imc < 18.5:
+        situacao = 'abaixo do peso'
+    elif imc >= 18.5 and imc < 25:
+        situacao = 'peso normal'
+    else:
+        situacao = 'acima do peso'
+    
+    print(f'Seu IMC é: {imc:.2f}')
+    print(f'Você está {situacao}.')
+    
+    
+if __name__ == '__main__':
+    main()
