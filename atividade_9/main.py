f5261cbe0810339cefec7db7c465a1761bb4e0d5
Nonolism <ruanvitormoreno@outlook.com>
2025-01-25 19:39:43 -0300

    Atv9 Verificando a paridade de um número

---
+++ atividade_9/main.py
@@ -0,0 +1,10 @@
+def main():
+    numero = int(input('Digite um número inteiro: '))
+    
+    if numero % 2 == 0:
+        print('O número é par.')
+    else:
+        print('O número é impar.')
+    
+if __name__ == '__main__':
+    main()
